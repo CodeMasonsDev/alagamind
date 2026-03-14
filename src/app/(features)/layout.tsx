1822a398bf030c8b09650110a47f0bbc68d768ae
CarlConradDeclaro <carlconrad98071234@gmail.com>
2026-03-14 19:44:00 +0800

    minor changes

--- src/app/(features)/layout.tsx
+++ src/app/(features)/layout.tsx
@@ -8,14 +8,11 @@ export default function FeaturesLayout({
 }) {
   return (
     <ReflectionsProvider>
-      {/* 'flex' puts them side-by-side; 'h-screen' ensures the layout fills the window */}
       <div className="flex h-screen bg-white">
         <aside className="h-full">
           <Sidebar />
         </aside>
 
-        {/* 'flex-1' makes this section grow to fill all remaining space */}
-        {/* 'overflow-y-auto' allows the dashboard to scroll independently of the sidebar */}
         <main className="flex-1 h-full overflow-y-auto  ">{children}</main>
       </div>
     </ReflectionsProvider>
